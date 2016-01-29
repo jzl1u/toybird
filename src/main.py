57a8e9d2fa524cf89989056c30c3c060636ab873
=w= <efantacy95@gmail.com>
2016-01-29 21:23:40 +0800

    2

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import math
+import time
+import pygame
+
+current_path = os.getcwd()
+sys.path.insert(0, os.path.join(current_path, "../pymuk-4.0.0"))
+
+#import pymunk as pm
+
+pygame.init()
+screen = pygame.display.set_mode((1200,650))
